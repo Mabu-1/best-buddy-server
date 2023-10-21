76ea3f44f850556df481f9ee94cafa357d649741
Mabu1 <umahtab65@gmail.com>
2023-10-21 19:05:59 +0600

    vercel added

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
     const userCollection = client.db("userDB").collection("products");
@@ -101,6 +101,15 @@ app.post("/client", async (req, res) => {
   res.send(result);
 });
 
+app.get('/client', async (req, res) => {
+  console.log(req.query.email);
+  let query = {};
+  if (req.query?.email) {
+    query = { email: req.query.email }
+  }
+  const result = await clientCollection.find(query).toArray();
+  res.send(result);
+})
 
 
     await client.db("admin").command({ ping: 1 });
@@ -112,7 +121,7 @@ app.post("/client", async (req, res) => {
 }
 run().catch(console.dir);
 app.get("/", (req, res) => {
-    res.send("Crud is running .....");
+    res.send("Crud is runnissng .....");
   });
   
   app.listen(port, () => {
